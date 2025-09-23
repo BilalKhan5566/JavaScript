36d5e2d3badc610be013ce1161d0a5da1ff63a17
Bilal Khan <bkbolty1122@example.com>
2025-09-23 18:46:52 +0500

    Variables

--- 01.js
+++ 01.js
@@ -1,3 +1,8 @@
+// how to upload the new files/changes to github repository!
+// git add .      (add everything that’s new or changed.)
+// git commit -m "some message"      (Use a short but clear message so you remember what changed.)
+// git push         (That sends the commit to the main branch on GitHub.)
+
 let name = "Bilal";
 console.log(`Hi ${name}, welcome to JavaScript!`);
 
---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 122553 
+// constant! can't change the value
+// accountId = 12345 will now result in error, becuase we already declare this constant
+let accountEmail = "Bkbolty1122@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mulatn"
+let Price;
+console.table([accountEmail, accountPassword, accountCity, accountId, Price]);
+// Assignment!
+// const birthyear = 2007
+// let age = 18
+// age += 1
+// console.log([birthyear, age]);
