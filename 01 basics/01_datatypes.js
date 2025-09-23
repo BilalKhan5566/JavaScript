f26dee0bea824409e29521aaa98b23a8745b2ba3
Bilal Khan <bkbolty1122@example.com>
2025-09-23 19:08:40 +0500

    Changes

---
+++ 01 basics/01_datatypes.js
@@ -0,0 +1,13 @@
+const numberig = 15
+const stringig = "Bilal Khan"
+var system = false
+let undefinedig;
+let nullig = null
+let bigintig = 123n
+console.log(typeof numberig);
+console.log(typeof stringig);
+console.log(typeof system);
+console.log(typeof undefinedig);
+console.log(typeof nullig);
+console.log(typeof bigintig);
+// this is both the assignment and the lecture!
