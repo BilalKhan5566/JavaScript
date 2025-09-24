441cdff1786aa11f5d5e5386a9b3345979bda0ab
Bilal Khan <bkbolty1122@example.com>
2025-09-24 21:47:18 +0500

    numberconversion

---
+++ 01 basics/01_numberconversion.js
@@ -0,0 +1,16 @@
+//lecture
+let num1 = 5
+console.log("5" + 2); // string * number → number
+console.log("5" * 2); // string + number → string (concatenation)
+console.log("5" - 2); // string - number → number
+console.log("5" + "2"); // string + string → number
+
+// //Assignment!
+// function safeAdd(x, y) 
+// {
+//   const num1 = Number(x);
+//   const num2 = Number(y);
+//   return num1 + num2;
+// }
+// const result = safeAdd("5", "10");
+// console.log(result); // 15
