0d31669b5f0f746659bdf06657823f32cd8d4997
Bilal Khan <bkbolty1122@example.com>
2025-09-25 20:25:56 +0500

    Comparison

---
+++ 01 basics/comparison.js
@@ -0,0 +1,15 @@
+// lecture
+console.log(2 == 1) // == → equal to 
+console.log("2" === 1) // === → checks if the datatype matches & also checks the statement. In this case it will be FALSE.
+console.log(2 === 1)
+console.log(2 != 1) // != → not equal to 
+console.log("2" > 1) 
+console.log("02" > 1)
+
+console.log(null == 0)
+console.log(null > 0)
+console.log(null < 0)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
